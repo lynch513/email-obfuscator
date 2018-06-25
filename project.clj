a3134f92f61628695477fa201c8b469b2bb4d3ba
lynch513 <lynch513@yandex.ru>
2018-06-25 16:43:09 +0300

    Fix error :url field in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.lynch/email-obfuscator "0.1.0-SNAPSHOT"
   :description "Clojure library for obfuscation e-mails. It combines some obfuscation technics"
-  :url "github.com:lynch513/email-obfuscator.git"
+  :url "http://github.com/lynch513/email-obfuscator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
